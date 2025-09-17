9d7ce9ba406f3b7dbe2d5caaefa9dcc594aa49ae
Adeel K <149257650+Akhatri98@users.noreply.github.com>
2025-09-17 12:45:56 -0400

    test

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+// src/components/Navbar.jsx
+import { NavLink } from "react-router-dom";
+export default function Navbar() {
+    return (
+        <nav>
+            <NavLink to="/">Home</NavLink>{" | "}
+            <NavLink to="/about">About</NavLink>{" | "}
+            <NavLink to="/contact">Contact</NavLink>
+        </nav>
+    );
+}
